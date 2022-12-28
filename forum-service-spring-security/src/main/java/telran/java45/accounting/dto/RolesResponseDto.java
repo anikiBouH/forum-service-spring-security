d5e5f56f8f45288ae83ae2f5126f92aa50741c58
aniki <aniki@aniki-Pulse>
2022-12-28 04:12:58 +0200

    added some tests

--- forum-service-spring-security/src/main/java/telran/java45/accounting/dto/RolesResponseDto.java
+++ forum-service-spring-security/src/main/java/telran/java45/accounting/dto/RolesResponseDto.java
@@ -4,17 +4,18 @@ import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Singular;
-import lombok.ToString;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
+@EqualsAndHashCode
 @Builder
 public class RolesResponseDto {
 	String login;
--- forum-service-spring-security/src/test/java/telran/java45/ForumServiceSpringSecurityApplicationTests.java
+++ forum-service-spring-security/src/test/java/telran/java45/ForumServiceSpringSecurityApplicationTests.java
@@ -9,12 +9,14 @@ import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 
 import java.util.List;
 import java.util.Optional;
 
 import telran.java45.accounting.dao.UserAccountRepository;
+import telran.java45.accounting.dto.RolesResponseDto;
 import telran.java45.accounting.dto.UserAccountResponseDto;
 import telran.java45.accounting.dto.UserRegisterDto;
 import telran.java45.accounting.dto.UserUpdateDto;
@@ -31,42 +33,82 @@ class ForumServiceSpringSecurityApplicationTests {
 	@Autowired
 	UserAccountServiceImpl userAccountServiceImpl;
 	
+	String login;
 	UserAccount javaFunAccount;
 	UserAccountResponseDto javaFunAccountResponseDto;
 	UserRegisterDto javaFunRegisterDto;
 	UserUpdateDto javaFunUpdateDto;
+	RolesResponseDto javaFunRolesResponseDto;
 	@BeforeEach
 	void setup(){
+		login = "JavaFun";
 		javaFunAccount = new UserAccount("JavaFun", "1234", "John", "Smith");
 		javaFunAccountResponseDto = UserAccountResponseDto.builder()
-														 .login("JavaFun")
-														 .firstName("John")
-														 .lastName("Smith")
-														 .roles(List.of("USER"))
-														 .build();
+				.login("JavaFun")
+				.firstName("John")
+				.lastName("Smith")
+				.roles(List.of("USER"))
+				.build();
 		javaFunRegisterDto = UserRegisterDto.builder()
-											.login("JavaFun")
-											.password("1234")
-											.firstName("John")
-											.lastName("Smith")
-											.build();
+				.login("JavaFun")
+				.password("1234")
+				.firstName("John")
+				.lastName("Smith")
+				.build();
 		
+		when(repository.existsById(any())).thenReturn(true);
 		when(repository.existsById("JavaFun")).thenReturn(false);
+		
+		when(repository.findById(any())).thenReturn(null);
 		when(repository.findById("JavaFun")).thenReturn(Optional.ofNullable(javaFunAccount));
 	}
 	
-	@Test
-	void existsById() {
-		
-	}
-	
+//	public UserAccountResponseDto addUser(UserRegisterDto userRegisterDto)
 	@Test
 	void addUser() {
 		UserAccountResponseDto expected = javaFunAccountResponseDto;
-		System.out.println(expected);
 		UserAccountResponseDto actual = userAccountServiceImpl.addUser(javaFunRegisterDto);
-		System.out.println(actual);
 		assertEquals(expected, actual);
 	}
+//	public UserAccountResponseDto getUser(String login)
+	@Test
+	void getUser() {
+		UserAccountResponseDto expected = javaFunAccountResponseDto;
+		UserAccountResponseDto actual = userAccountServiceImpl.getUser(login);
+		assertEquals(expected, actual);
+	}
+//	public UserAccountResponseDto removeUser(String login)
+	@Test
+	void removeUser() {
+		UserAccountResponseDto expected = javaFunAccountResponseDto;
+		UserAccountResponseDto actual = userAccountServiceImpl.removeUser(login);
+		assertEquals(expected, actual);
+	}
+//	public UserAccountResponseDto editUser(String login, UserUpdateDto userUpdateDto)
+	void editUser() {
+		javaFunUpdateDto = UserUpdateDto.builder()
+				.firstName("Johnnn")
+				.lastName("Smith")
+				.build();
+		UserAccountResponseDto expected = javaFunAccountResponseDto;
+		expected.setFirstName(javaFunUpdateDto.getFirstName());
+		expected.setLastName(javaFunUpdateDto.getLastName());
+		UserAccountResponseDto actual = userAccountServiceImpl.editUser(login, javaFunUpdateDto);
+		assertEquals(expected, actual);
+	}
+//	public RolesResponseDto changeRolesList(String login, String role, boolean isAddRole)
+	void changeRolesList() {
+		javaFunRolesResponseDto = RolesResponseDto.builder()
+				.login(login)
+				.role("USER")
+				.role("ADMINISTRATOR")
+				.build();
+		RolesResponseDto expected = javaFunRolesResponseDto;
+		RolesResponseDto actual = userAccountServiceImpl.changeRolesList(login, "ADMINISTRATOR", true);
+		assertEquals(expected, actual);
+	}
+//	public void changePassword(String login, String newPassword)
+	
+	
 
 }
